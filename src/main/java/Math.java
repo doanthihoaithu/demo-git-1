9ab94dee7ae44e3d2f8f7b0f483a428576e6e86c
doanthihoaithu <hoaithuxt1998@gmail.com>
2021-09-29 00:16:20 +0700

    add gitignore

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -3,7 +3,12 @@ public class Math {
         return a + b;
     }
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
 }
